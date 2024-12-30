7d7918ddd40a7da0b6c23fcb0d66d9c1dac1b48a
Mansurkhoja <mansur.atm@gmail.com>
2024-12-30 20:05:33 +0500

    team names

--- src/scripts/team.ts
+++ src/scripts/team.ts
@@ -1,17 +1,17 @@
 import { gsap } from 'gsap'
 import { bodyScroll } from './app'
 
-const elList = document.querySelector('.team__list') as HTMLElement
-const elItems = elList.querySelectorAll(
+const list = document.querySelector('.team__list') as HTMLElement
+const items = list.querySelectorAll(
 	'.team__item'
 ) as NodeListOf<HTMLElement>
-const elItemsBg = elList.querySelectorAll(
+const itemsBg = list.querySelectorAll(
 	'.team__bg'
 ) as NodeListOf<HTMLElement>
-const elAvatarContainer = document.querySelector(
+const avatarContainer = document.querySelector(
 	'.team__avatars'
 ) as HTMLElement
-const elAvatars = elAvatarContainer.querySelectorAll(
+const avatars = avatarContainer.querySelectorAll(
 	'.team__avatar'
 ) as NodeListOf<HTMLElement>
 
@@ -21,10 +21,10 @@ let clientYList = 0
 let avatarScrollEventStarted = false
 let isMouseEnteredList = false
 
-const setAvatarContainerX = gsap.quickTo(elAvatarContainer, 'x', {
+const setAvatarContainerX = gsap.quickTo(avatarContainer, 'x', {
 	ease: 'power3',
 })
-const setAvatarContainerY = gsap.quickTo(elAvatarContainer, 'y', {
+const setAvatarContainerY = gsap.quickTo(avatarContainer, 'y', {
 	ease: 'power3',
 })
 
@@ -35,13 +35,13 @@ const setAvatarContainerPosition = (x: number, y: number) => {
 
 const setAvatarContainerMaxYX = () => {
 	xAvatarContainerMax =
-		(window.innerWidth - elAvatarContainer.clientWidth) / window.innerWidth
+		(window.innerWidth - avatarContainer.clientWidth) / window.innerWidth
 	yAvatarContainerMax =
-		(window.innerHeight - elAvatarContainer.clientHeight) / window.innerHeight
+		(window.innerHeight - avatarContainer.clientHeight) / window.innerHeight
 }
 
 const currentYList = () =>
-	clientYList * yAvatarContainerMax - elList.getBoundingClientRect().top
+	clientYList * yAvatarContainerMax - list.getBoundingClientRect().top
 
 const onMouseMoveList = (e: MouseEvent) => {
 	clientYList = e.clientY
@@ -57,42 +57,42 @@ export default () => {
 	setAvatarContainerMaxYX()
 	window.addEventListener('resize', setAvatarContainerMaxYX)
 
-	gsap.set(elItemsBg, {
+	gsap.set(itemsBg, {
 		yPercent: 100,
 	})
 
-	elItems.forEach((item, index) => {
+	items.forEach((item, index) => {
 		item.addEventListener('mouseenter', e => {
 			const halfHeight = item.getBoundingClientRect().height / 2
 
-			gsap.set(elItemsBg[index], {
+			gsap.set(itemsBg[index], {
 				yPercent: itemBgYPercent(e, halfHeight),
 			})
-			gsap.to(elItemsBg[index], {
+			gsap.to(itemsBg[index], {
 				yPercent: 0,
 				duration: 0.4,
 				ease: 'power2.out',
 			})
 			item.classList.add('active')
-			elAvatars[index].classList.add('active')
+			avatars[index].classList.add('active')
 		})
 
 		item.addEventListener('mouseleave', e => {
 			const halfHeight = item.getBoundingClientRect().height / 2
 
-			gsap.to(elItemsBg[index], {
+			gsap.to(itemsBg[index], {
 				yPercent: itemBgYPercent(e, halfHeight),
 				duration: 0.4,
 				ease: '0.22, 0.6, 0.36, 1',
 			})
 			item.classList.remove('active')
-			elAvatars[index].classList.remove('active')
+			avatars[index].classList.remove('active')
 		})
 	})
 
-	elList.addEventListener('mousemove', onMouseMoveList)
+	list.addEventListener('mousemove', onMouseMoveList)
 
-	elList.addEventListener('mouseenter', e => {
+	list.addEventListener('mouseenter', e => {
 		isMouseEnteredList = true
 		onMouseMoveList(e)
 		if (!avatarScrollEventStarted) {
@@ -105,7 +105,7 @@ export default () => {
 		}
 	})
 
-	elList.addEventListener('mouseleave', () => {
+	list.addEventListener('mouseleave', () => {
 		isMouseEnteredList = false
 	})
 }
