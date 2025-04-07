270d6be0daf783590ebf019c401f53242e90aa00
Mansurkhoja <mansur.atm@gmail.com>
2025-04-07 17:35:11 +0500

    added slider to modal model

--- src/scripts/modal.ts
+++ src/scripts/modal.ts
@@ -1,5 +1,13 @@
 import { gsap } from 'gsap'
 import { bodyScroll } from './app'
+import Slider from './slider'
+
+interface ModalSliders {
+	[key: string]: {
+		slider: Slider
+	}
+}
+
 const modalShowName = 'data-modal-show'
 const modalHideName = 'data-modal-hide'
 
@@ -26,19 +34,23 @@ export const show = (id: string | null) => {
 	}
 }
 
-const hide = (id: string | null) => {
+const hide = (id: string | null, onHide: () => void | undefined) => {
 	tl.timeScale(2)
 		.reverse()
 		.eventCallback('onReverseComplete', () => {
+			if (onHide) onHide()
 			if (activeModal) {
 				activeModal.querySelector('.modal__inner')?.scroll(0, 0)
 			}
+			activeModal = null
 			bodyScroll.start()
 			show(id)
 		})
 }
 
 export default () => {
+	const modalSliders: ModalSliders = {}
+
 	gsap.registerEffect({
 		name: 'modal',
 		effect: (modal: HTMLElement, container: HTMLElement) => {
@@ -59,6 +71,14 @@ export default () => {
 	showButtons.forEach(button => {
 		const id = button.getAttribute(modalShowName)
 		button.addEventListener('click', () => {
+			if (id && modalSliders[id]) {
+				const slider = modalSliders[id].slider
+				if (!slider.initialized) {
+					slider.show()
+				} else {
+					slider.startSlider()
+				}
+			}
 			show(id)
 		})
 	})
@@ -66,7 +86,50 @@ export default () => {
 	hideButtons.forEach(button => {
 		const id = button.getAttribute(modalHideName)
 		button.addEventListener('click', () => {
-			hide(id)
+			hide(id, () => {
+				const activeModalId = activeModal?.getAttribute('id')
+				if (activeModalId && modalSliders[activeModalId]) {
+					const slider = modalSliders[activeModalId].slider
+					if (slider.initialized) {
+						slider.stopSlider()
+					}
+				}
+			})
 		})
+	})
+
+	const sliders = document.querySelectorAll('.modal__slider')
+
+	sliders.forEach(item => {
+		const modalElement = item.closest('.modal') as HTMLElement
+		const modalId = modalElement.getAttribute('id')
+
+		if (modalId) {
+			const sliderContainer = item.querySelector(
+				'.modal__slider-container'
+			) as HTMLElement
+			const slides = item.querySelectorAll(
+				'.modal__slide'
+			) as NodeListOf<HTMLElement>
+
+			const slider = new Slider({
+				section: modalElement,
+				parent: sliderContainer,
+				slides: slides,
+				speed: 1.2,
+				easing: 'power1.in',
+				autoInit: false,
+				emitOnShown() {
+					const activeModalId = activeModal?.getAttribute('id')
+					if (modalId === activeModalId) {
+						slider.startSlider()
+					}
+				},
+			})
+
+			modalSliders[modalId] = {
+				slider,
+			}
+		}
 	})
 }
